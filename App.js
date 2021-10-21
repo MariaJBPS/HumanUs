bd5df4e94cc7fee8be6b545e81ba193f4a79e66c
Victor Charles-Nwufoh <vc248@kent.ac.uk>
2021-10-21 14:53:07 +0100

    1st commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text pushed");
+
   return (
+    // View is for the app UI view
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={handlePress}>CRAZY TIME that is very long to see what will happen but its crazy so I move ike a mad man for no reason</Text>
+      <Image source={require/('./assets/icon.png')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
+  
 });
