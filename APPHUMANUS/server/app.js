16d41854d53560589dba03a77f32931f9b62b641
Maria Santos <mb2007@kent.ac.uk>
2021-11-25 11:04:12 +0000

    .

--- APPHUMANUS/server/app.js
+++ APPHUMANUS/server/app.js
@@ -1,1 +1,24 @@
-console.log("this is app.js");
+const express = require('express');
+
+const sequelize = require('./utils/database.js');
+
+const router = reuire('./routes/routes.js'); 
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use((_, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use(router);
+
+sequelize.sync(); 
+
+app.listen(5000); // port where the server will be listening for requests
