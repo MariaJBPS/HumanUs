b9a5db229b6981a6a2c4850e21d2d38ec46e60ee
Maria Santos <mb2007@kent.ac.uk>
2022-01-26 19:32:41 +0000

    .

--- APPHUMANUS/server/utils/database.js
+++ APPHUMANUS/server/utils/database.js
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize"; // create the database connection
 
 const sequelize = new Sequelize("loginDB", "root", "root", {
+  //dbname=mb2007", "mb2007", "weitab6"
   dialect: "mysql",
-  host: "localhost",
-  port: "3306",
-  logging: true,
+  host: "localhost", //dragon.kent.ac.uk
+  port: "3306", //remove this?
+  logging: true, // remove this?
 });
 
 sequelize
@@ -12,4 +13,4 @@ sequelize
   .then(() => console.log("db connected"))
   .catch((e) => console.log(`db error ${e}`));
 
-export default sequelize; //use this syntax instead:
+export default sequelize;
