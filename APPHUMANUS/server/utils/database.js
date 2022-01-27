f2d962ffe7da5696d58b4c9ebac267b031a9ac29
Maria Santos <mb2007@kent.ac.uk>
2022-01-27 11:35:29 +0000

    new remote database

--- APPHUMANUS/server/utils/database.js
+++ APPHUMANUS/server/utils/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize"; // create the database connection
 
-const sequelize = new Sequelize("mb2007", "mb2007", "weitab6", {
+const sequelize = new Sequelize("comp6000_11", "comp6000_11", "yp1olyb", {
   dialect: "mysql",
   host: "dragon.kent.ac.uk",
 });
